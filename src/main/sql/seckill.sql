6d73517dbbf304f9e7e4ff6bbfd29a2096580727
lj360560179 <360560179@qq.com>
2016-07-29 11:36:17 +0800

    2016.7.29 根据http://my.oschina.net/crossoverjie/blog/712373 将 shiro 整合到项目中

--- src/main/sql/seckill.sql
+++ src/main/sql/seckill.sql
@@ -60,4 +60,65 @@ call execute_seckill(1001,15067139479,now(),@r_result);
 -- 4: qps:一个秒杀单6000/qps
 
 
+-- 2016.7.29shiro整合新增表以及相关数据
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for t_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `t_permission`;
+CREATE TABLE `t_permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `permissionName` varchar(50) DEFAULT NULL,
+  `roleId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `roleId` (`roleId`),
+  CONSTRAINT `t_permission_ibfk_1` FOREIGN KEY (`roleId`) REFERENCES `t_role` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_permission
+-- ----------------------------
+INSERT INTO `t_permission` VALUES ('1', 'user:*', '1');
+INSERT INTO `t_permission` VALUES ('2', 'student:*', '2');
+
+-- ----------------------------
+-- Table structure for t_role
+-- ----------------------------
+DROP TABLE IF EXISTS `t_role`;
+CREATE TABLE `t_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `roleName` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_role
+-- ----------------------------
+INSERT INTO `t_role` VALUES ('1', 'admin');
+INSERT INTO `t_role` VALUES ('2', 'teacher');
+
+-- ----------------------------
+-- Table structure for t_user
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(20) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `roleId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `roleId` (`roleId`),
+  CONSTRAINT `t_user_ibfk_1` FOREIGN KEY (`roleId`) REFERENCES `t_role` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_user
+-- ----------------------------
+INSERT INTO `t_user` VALUES ('1', 'crossoverJie', '123456', '1');
+INSERT INTO `t_user` VALUES ('2', 'aaa', '12345', '2');
+INSERT INTO `t_user` VALUES ('3', 'bbb', '12345', null);
+INSERT INTO `t_user` VALUES ('4', 'ccc', '12345', null);
+
+
 
